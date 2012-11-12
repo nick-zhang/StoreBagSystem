cb340f5db57f133c2a5c9f0657e7e8763706d8ca
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-11-12 15:01:18 +0800

    Store the Bag by SmartRobot into cabinet with most avaiable boxes.

--- StoreBagSystemTest/SmartRobotTest.cs
+++ StoreBagSystemTest/SmartRobotTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoreBagSystem;
 
@@ -15,15 +16,25 @@ namespace StoreBagSystemTest
             var cabinets = new List<Cabinet> { cabinet1, cabinet2 };
 
             var smartRobot = new SmartRobot(cabinets);
+            smartRobot.Store(new Bag());
+            Assert.AreEqual(1, cabinet1.AvailableBoxes());
             Assert.AreEqual(1, cabinet2.AvailableBoxes());
         }
     }
 
     public class SmartRobot
     {
+        private readonly IList<Cabinet> cabinets;
+
         public SmartRobot(IList<Cabinet> cabinets)
         {
-            throw new System.NotImplementedException();
+            this.cabinets = cabinets;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            var cabinetWithMostAvailableBoxes = cabinets.OrderBy(cabinet => cabinet.AvailableBoxes()).Last();
+            return cabinetWithMostAvailableBoxes.Store(bag);
         }
     }
 }
