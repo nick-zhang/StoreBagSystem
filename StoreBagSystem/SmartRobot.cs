79ec668afb78f8a1ab57be0de38de2991ad8dd19
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-11-12 16:30:01 +0800

    ShouldPickBagSuccessfullyBySmartRobot

--- StoreBagSystem/SmartRobot.cs
+++ StoreBagSystem/SmartRobot.cs
@@ -17,5 +17,10 @@ namespace StoreBagSystem
             var cabinetWithMostAvailableBoxes = cabinets.OrderByDescending(cabinet => cabinet.AvailableBoxes()).First();
             return cabinetWithMostAvailableBoxes.Store(bag);
         }
+
+        public Bag Pick(Ticket ticket)
+        {
+            return cabinets.Select(cabinet => cabinet.Pick(ticket)).FirstOrDefault(bag => bag != null);
+        }
     }
 }
--- StoreBagSystemTest/SmartRobotTest.cs
+++ StoreBagSystemTest/SmartRobotTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoreBagSystem;
 
@@ -12,7 +13,7 @@ namespace StoreBagSystemTest
         {
             var cabinet1 = new Cabinet(1);
             var cabinet2 = new Cabinet(2);
-            var cabinets = new List<Cabinet> { cabinet1, cabinet2 };
+            var cabinets = new List<Cabinet> {cabinet1, cabinet2};
             var smartRobot = new SmartRobot(cabinets);
             smartRobot.Store(new Bag());
             Assert.AreEqual(1, cabinet1.AvailableBoxes());
@@ -24,7 +25,7 @@ namespace StoreBagSystemTest
         {
             var cabinet1 = new Cabinet(1);
             var cabinet2 = new Cabinet(1);
-            var cabinets = new List<Cabinet> { cabinet1, cabinet2 };
+            var cabinets = new List<Cabinet> {cabinet1, cabinet2};
 
             var smartRobot = new SmartRobot(cabinets);
             smartRobot.Store(new Bag());
@@ -33,14 +34,28 @@ namespace StoreBagSystemTest
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CabinetException), "No available box exception!")]
+        [ExpectedException(typeof (CabinetException), "No available box exception!")]
         public void ShouldThrowExceptionWhenNoAvailableBox()
         {
             var cabinet1 = new Cabinet(0);
             var cabinet2 = new Cabinet(0);
-            var cabinets = new List<Cabinet> { cabinet1, cabinet2 };
+            var cabinets = new List<Cabinet> {cabinet1, cabinet2};
             var smartRobot = new SmartRobot(cabinets);
             smartRobot.Store(new Bag());
+        }
+
+        [TestMethod]
+        public void ShouldPickBagSuccessfullyBySmartRobot()
+        {
+            var cabinet1 = new Cabinet(1);
+            var cabinet2 = new Cabinet(2);
+            var cabinets = new List<Cabinet> { cabinet1, cabinet2 };
+            var smartRobot = new SmartRobot(cabinets);
+            var bag = new Bag();
+            var ticket = smartRobot.Store(bag);
+
+            var pickedBag = smartRobot.Pick(ticket);
+            Assert.AreSame(bag, pickedBag);
         }
     }
 }
